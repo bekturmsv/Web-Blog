42f09667ca0d60e2ebdfccde7bc835540e8a3ee9
Bektur211 <bekamaasaliev@gmail.com>
2023-01-12 02:03:17 +0600

    connected to MongoDB using mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 import express from "express"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb+srv://admin:21123145@cluster0.olilxg5.mongodb.net/?retryWrites=true&w=majority")
+.then(()=> {
+    console.log("DB is connected");
+})
+.catch((err)=> {
+    console.log("DB error"+err);
+})
 
 const app = express()
 
@@ -13,9 +22,14 @@ app.get("/", (req,res) => {
 app.post("/auth/login", (req,res)=> {
     console.log(req.body);
 
-    
+    const token = jwt.sign({
+        email: req.body.email,
+        fullname: "Бека Маасалиев"
+    }, "hash")
+
     res.json({
-        success:true
+        success:true,
+        token
     })
 })
 
