83ed5ed7d08e6b2e8cabbbf330498eddff7aab4c
Bektur211 <bekamaasaliev@gmail.com>
2023-01-12 02:24:48 +0600

    create validation for user register

---
+++ server/validations/auth.js
@@ -0,0 +1,9 @@
+import {body} from "express-validator"
+
+export const registerValidation =[
+    body("email", "Неверный формат почты").isEmail(),
+    body("password","Пароль должен быть минимум 5 символов").isLength({min: 5}),
+    body("fullName","Укажите имя").isLength({min: 3}),
+    body("avatarUrl","Неверная ссылка на аватарку").optional().isURL(),
+
+]
