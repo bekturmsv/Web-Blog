9ab268663220882dd1852176e5350650e2ec665d
Bektur211 <bekamaasaliev@gmail.com>
2023-01-12 02:32:03 +0600

    hash password from client, create new user in database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
+import bcrypt from "bcrypt "
 import { validationResult } from "express-validator"
 import { registerValidation } from "./validations/auth.js"
+import UserModel from "./models/User.js"
 
 mongoose.connect("mongodb+srv://admin:21123145@cluster0.olilxg5.mongodb.net/?retryWrites=true&w=majority")
 .then(()=> {
@@ -17,14 +19,30 @@ const app = express()
 app.use(express.json())
 
 
-app.post("/auth/register",registerValidation, (req,res)=> {
+app.post("/auth/register",registerValidation, async (req,res)=> {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json(errors.array())
     }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash
+    })
+
+    const user = await doc.save();
+
     res.json({
         success:true
     })
+
+
 })
 
 app.listen(5000, (err)=>{
