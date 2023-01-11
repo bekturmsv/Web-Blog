0fa8c61770467f9cfc48454c7db245169290b304
Bektur211 <bekamaasaliev@gmail.com>
2023-01-12 01:47:29 +0600

    new post request for login, adding jwt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 import express from "express"
+import jwt from "jsonwebtoken"
 
 const app = express()
 
+app.use(express.json())
+
+
 app.get("/", (req,res) => {
-    res.send("Hello World")
+    res.send("Hello World!!")
+})
+
+app.post("/auth/login", (req,res)=> {
+    console.log(req.body);
+
+    
+    res.json({
+        success:true
+    })
+})
+
+app.listen(5000, (err)=>{
+    if(err){
+        return console.log(err);
+    }
+    console.log("Server started without problems");
 })
