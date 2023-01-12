bde2263ad23080344db4a47aa70ef68f07d140d5
Bektur211 <bekamaasaliev@gmail.com>
2023-01-13 00:43:21 +0600

    added the ability to log into an account in the app

--- server/index.js
+++ server/index.js
@@ -18,6 +18,43 @@ const app = express()
 
 app.use(express.json())
 
+app.post("/auth/login", async (req,res)=> {
+    try{
+        const user = await UserModel.findOne({email: req.body.email});
+
+        if(!user){
+            return req.status(404).json({
+                message: "Пользователь не найден"
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if(!isValidPass){
+            return res.status(400).json({
+                message: "Неверный логин или пароль"
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id
+        },"hash",{
+            expiresIn: "30d"
+        })
+
+        const {passwordHash, ...userData } = user._doc
+
+    res.json({
+        ...userData,
+        token
+    })
+    }catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: "Не удалось авторизоваться",
+        })
+    }
+})
 
 app.post("/auth/register",registerValidation, async (req,res)=> {
     
