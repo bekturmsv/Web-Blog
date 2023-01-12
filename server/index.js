eb1bd286b6f6bb45e2563e32ef279f22da02a70e
Bektur211 <bekamaasaliev@gmail.com>
2023-01-13 00:23:22 +0600

    refactor regster code, add jwt in registration

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ app.use(express.json())
 
 
 app.post("/auth/register",registerValidation, async (req,res)=> {
+    
+try{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json(errors.array())
@@ -27,19 +29,35 @@ app.post("/auth/register",registerValidation, async (req,res)=> {
 
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
+    const hash = await bcrypt.hash(password, salt)
 
     const doc = new UserModel({
         email: req.body.email,
         fullName: req.body.fullName,
         avatarUrl: req.body.avatarUrl,
-        passwordHash
+        passwordHash : hash
     })
 
     const user = await doc.save();
 
-    res.json(user)
+    const token = jwt.sign({
+        _id: user._id
+    },"hash",{
+        expiresIn: "30d"
+    })
 
+    const {passwordHash, ...userData } = user._doc
+
+    res.json({
+        ...userData,
+        token
+    })
+}catch(err){
+    console.log(err);
+    res.status(500).json({
+        message: "Не удалось зарегистрироваться",
+    })
+}
 
 })
 
