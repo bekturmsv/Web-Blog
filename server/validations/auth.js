8a7ce7fff9afecaf3f3d184f95b0c9423cd1edf7
Bektur211 <bekamaasaliev@gmail.com>
2023-01-14 23:03:31 +0600

    changing min password length from 5 to 7

--- server/validations/auth.js
+++ server/validations/auth.js
@@ -5,5 +5,4 @@ export const registerValidation =[
     body("password","Пароль должен быть минимум 5 символов").isLength({min: 5}),
     body("fullName","Укажите имя").isLength({min: 3}),
     body("avatarUrl","Неверная ссылка на аватарку").optional().isURL(),
-
 ]
