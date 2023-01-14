f0adeb99da8f07915912c974cc5cbb613a396b1d
Bektur211 <bekamaasaliev@gmail.com>
2023-01-15 02:51:05 +0600

    create method that find and return only  one post

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -12,6 +12,43 @@ export const getAll = async(req,res)=> {
     }
 }
 
+export const getOne = async(req,res)=> {
+    try {
+        const postId = req.params.id;
+        
+        PostModel.findOneAndUpdate({
+                _id:postId
+        },{
+            $inc: { viewsCount : 1}
+        },{
+            returnDocument: "after"
+        },
+            (err,doc)=> {
+                if(err){
+                    console.log(error);
+                   return res.status(500).json({
+                        message: "Не удалось вернуть статью"
+                    })
+                }
+                 
+                if(!doc){
+                    return res.status(404).json({
+                        message: "Статья не найдена"
+                    })
+                }
+
+                res.json(doc)
+            }
+        )
+
+    } catch (error) {
+        console.log(error);
+            res.status(500).json({
+                message: "Не удалось создать статью"
+            })
+    }
+}
+
 export const create = async (req,res)=> {
         try {
             const doc = new PostModel({
