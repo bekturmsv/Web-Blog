02f42c864fabead06e8225494b600802dcf76fdb
Bektur211 <bekamaasaliev@gmail.com>
2023-01-15 02:42:26 +0600

    create routes for get all posts, and create post

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose"
 import { registerValidation, loginValidation } from "./validations/auth.js"
 import chekAuth from "./utils/chekAuth.js";
 import * as UserController from "./controllers/UserController.js"
+import * as PostController from "./controllers/PostController.js"
+import { postCreateValidation } from "./validations/post.js";
 
 mongoose.connect("mongodb+srv://admin:21123145@cluster0.olilxg5.mongodb.net/Web-Blog?retryWrites=true&w=majority")
 .then(()=> {
@@ -19,6 +21,13 @@ app.use(express.json())
 app.post("/auth/login",loginValidation, UserController.login )
 app.post("/auth/register",registerValidation, UserController.register )
 app.get("/auth/profile",chekAuth, UserController.getUserInfo)
+
+app.get("/posts",PostController.getAll)
+// app.get("/posts/:id", PostController.getOne)
+app.post("/posts", chekAuth, postCreateValidation, PostController.create)
+// app.delete("/posts", PostController.remove)
+// app.patch("/posts", PostController.update)
+
 
 app.listen(5000, (err)=>{
     if(err){
