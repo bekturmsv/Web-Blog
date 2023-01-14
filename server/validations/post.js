8da4d6070df9d2ce76a0cc5fea3fe9f4e4899fda
Bektur211 <bekamaasaliev@gmail.com>
2023-01-15 02:40:18 +0600

    create validation for post creating

---
+++ server/validations/post.js
@@ -0,0 +1,8 @@
+import {body} from "express-validator"
+
+export const postCreateValidation =[
+    body("title", "Введите заголовок статьи").isLength({min:3}).isString(),
+    body("text","Введите текст статьи").isLength({min: 10}).isString(),
+    body("tags","Неверный формат тэгов (укажите массив)").optional().isString(),
+    body("imageUrl","Неверная ссылка на изображение").optional().isString(),
+]
