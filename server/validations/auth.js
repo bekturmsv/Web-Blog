049b48b849b31a9c6d2138bcd40ec7e1e505b412
Bektur211 <bekamaasaliev@gmail.com>
2023-01-14 23:04:35 +0600

    changing password length from 5 to 7

--- server/validations/auth.js
+++ server/validations/auth.js
@@ -2,7 +2,7 @@ import {body} from "express-validator"
 
 export const registerValidation =[
     body("email", "Неверный формат почты").isEmail(),
-    body("password","Пароль должен быть минимум 5 символов").isLength({min: 5}),
+    body("password","Пароль должен быть минимум 7 символов").isLength({min: 7}),
     body("fullName","Укажите имя").isLength({min: 3}),
     body("avatarUrl","Неверная ссылка на аватарку").optional().isURL(),
 ]
