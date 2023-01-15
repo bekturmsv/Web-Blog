4649d1a60f77c64ade86eb4bd4d4337229fe08b1
Bektur211 <bekamaasaliev@gmail.com>
2023-01-16 01:58:13 +0600

    add Routes to components

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,20 @@ import Container from "@mui/material/Container";
 
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+        <Routes>
+          <Route path="/" element={<Home/>}/> 
+          <Route path="/posts/:id" element={<FullPost/>} />
+          <Route path="/add-post" element={<AddPost/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Registration/>} />
+        </Routes>
       </Container>
     </>
   );
