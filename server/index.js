5b065fbc8e485433dc7d03b2ce8351c701aed41f
Bektur211 <bekamaasaliev@gmail.com>
2023-01-15 20:13:35 +0600

    return static images, in get request to this image

--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage})
 
 app.use(express.json())
+app.use("/uploads", express.static("uploads"))
 
 app.post("/auth/login",loginValidation, UserController.login )
 app.post("/auth/register",registerValidation, UserController.register )
