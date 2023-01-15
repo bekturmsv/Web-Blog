3e26c2bcd4a4fd743ff3f5157022984d1b6a44a1
Bektur211 <bekamaasaliev@gmail.com>
2023-01-16 02:05:53 +0600

    fix mistake with url path

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -55,7 +55,7 @@ export const Post = ({
         <UserInfo {...user} additionalText={createdAt} />
         <div className={styles.indention}>
           <h2 className={clsx(styles.title, { [styles.titleFull]: isFullPost })}>
-            {isFullPost ? title : <Link href={`/posts/${_id}`}>{title}</Link>}
+            {isFullPost ? title : <Link to={`/posts/${_id}`}>{title}</Link>}
           </h2>
           <ul className={styles.tags}>
             {tags.map((name) => (
