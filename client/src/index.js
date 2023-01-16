5b29cf56ededfe76e258d0a0065089fa507bfcd1
Bektur211 <bekamaasaliev@gmail.com>
2023-01-16 22:43:59 +0600

    wrap App.js with Provider from redux-toolkit, and sending to Provider store.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom"
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-
+import { Provider } from "react-redux";
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,8 +16,9 @@ root.render(
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-      
-      <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
