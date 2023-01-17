82e465d8fd8f75f5e013ee4cd924f5fcc0842f4f
Bektur211 <bekamaasaliev@gmail.com>
2023-01-17 22:27:43 +0600

    create action for fething posts from server

--- client/src/redux/slices/posts.js
+++ client/src/redux/slices/posts.js
@@ -1,4 +1,11 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import axios from "axios"
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+    const {data} = await axios.get("/posts");
+    console.log(data);
+    return data;
+})
 
 const initialState = {
     posts: {
