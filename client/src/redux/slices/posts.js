6fbec061c57bad37110ddf317064907fc903d18c
Bektur211 <bekamaasaliev@gmail.com>
2023-01-18 01:02:53 +0600

    added a new action fetchTags to pull tags from the server

--- client/src/redux/slices/posts.js
+++ client/src/redux/slices/posts.js
@@ -6,6 +6,12 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
     return data;
 })
 
+export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
+    const {data} = await axiosBaseUrl.get("/tags");
+    return data;
+})
+
+
 const initialState = {
     posts: {
         items: [],
@@ -33,6 +39,18 @@ const postSlice = createSlice({
         [fetchPosts.rejected]: (state)=> {
             state.posts.items = []
             state.posts.status = "error"
+        },
+        [fetchTags.pending]: (state)=> {
+            state.tags.items = []
+            state.tags.status = "loading"
+        },
+        [fetchTags.fulfilled]: (state,action)=> {
+            state.tags.items = action.payload
+            state.tags.status = "loaded"
+        },
+        [fetchTags.rejected]: (state)=> {
+            state.tags.items = []
+            state.tags.status = "error"
         },
     }
 })
