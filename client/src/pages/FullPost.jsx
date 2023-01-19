3422dfda37fdd296f781a3d53a1f017e8647efd9
Bektur211 <bekamaasaliev@gmail.com>
2023-01-19 19:28:06 +0600

    fetch details from choosen post by id

--- client/src/pages/FullPost.jsx
+++ client/src/pages/FullPost.jsx
@@ -1,33 +1,49 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import  {useParams} from "react-router-dom"
 import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
+import axiosBaseUrl from "../axios";
+
 
 export const FullPost = () => {
+  const [data,setData] = useState()
+  const [isLoading, setLoading] = useState(true )
+  const {id} = useParams();
+
+  useEffect(()=> {
+    axiosBaseUrl
+    .get(`/posts/${id}`)
+    .then(res => {
+      setData(res.data)
+      setLoading(false)
+    })
+    .catch(err => {
+      console.warn(err);
+      alert("Ошибка при получении статьи")
+    })
+  },[])
+
+  if(isLoading){
+    return <Post isLoading={isLoading} isFullPost /> 
+  }
+
   return (
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
-        }}
-        createdAt={"12 июня 2022 г."}
-        viewsCount={150}
+        id={data._id}
+        title={data.title}
+        imageUrl={data.imageUrl}
+        // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
         commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        tags={data.tags}
         isFullPost
       >
         <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
+        {data.text}
         </p>
       </Post>
       <CommentsBlock
