8722c21fe53a9da06f7a282323b027cef1e93725
Bektur211 <bekamaasaliev@gmail.com>
2023-01-19 19:28:40 +0600

    add post image url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,7 +37,7 @@ export const Home = () => {
               <Post
                 id={obj._id}
                 title={obj.title}
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                imageUrl={obj.imageUrl}
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
