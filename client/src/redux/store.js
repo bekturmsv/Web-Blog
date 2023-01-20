0d4aa5c4fed7a489351d56c177b560e6f0dce86c
Bektur211 <bekamaasaliev@gmail.com>
2023-01-20 06:56:51 +0600

    add new reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit" 
 import { postsReducer } from "./slices/posts";
-
+import { authReducer } from "./slices/auth";
 const store = configureStore({
     reducer: {
-        posts: postsReducer
+        posts: postsReducer,
+        auth: authReducer
     }
 })
 
