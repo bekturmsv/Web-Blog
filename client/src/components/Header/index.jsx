7032dd52280ae60d3bf883281d5a697021e159e1
Bektur211 <bekamaasaliev@gmail.com>
2023-01-21 01:01:12 +0600

    calling action logout by clicking on a button

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -3,13 +3,19 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
-import { useSelector } from 'react-redux';
-import { selectIsAuth } from '../../redux/slices/auth';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
 
 export const Header = () => {
+  const dispatch = useDispatch()
   const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if(window.confirm("Вы уверены что хотите выйти?")){
+    dispatch(logout())
+    }
+    
+  };
 
   return (
     <div className={styles.root}>
