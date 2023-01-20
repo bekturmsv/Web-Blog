e533e103e6e98b335040d58eea7d15a75b162098
Bektur211 <bekamaasaliev@gmail.com>
2023-01-20 06:56:31 +0600

    create new slice for user authentication, create action that get all user info

---
+++ client/src/redux/slices/auth.js
@@ -0,0 +1,33 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import axiosBaseUrl from "../../axios";
+
+export const fetchAuth = createAsyncThunk("auth/fetchAuth", async (params) => {
+    const {data} = await axiosBaseUrl.post("/auth/login", params);
+    return data;
+})
+
+const initialState = {
+    data: null,
+    status: "loading"
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    extraReducers:{
+        [fetchAuth.pending]: (state)=> {
+            state.status = "loading"
+            state.data = null
+        },
+        [fetchAuth.fulfilled]: (state,action)=> {
+            state.status = "laoded"
+            state.data = action.payload
+        },
+        [fetchAuth.rejected]: (state)=> {
+            state.status = "error"
+            state.data = null
+        },
+    }
+})
+
+export const authReducer = authSlice.reducer
