8b3b381ec762fc0c1ff93dd961a7d509b5f2106c
Bektur211 <bekamaasaliev@gmail.com>
2023-01-21 01:25:19 +0600

    add a token to the Authorization each time a request is mead

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,3 +1,10 @@
 import axios from 'axios';
 const axiosBaseUrl = axios.create({baseURL: 'http://localhost:5000'});
+
+axiosBaseUrl.interceptors.request.use((config)=> {
+    config.headers.Authorization = window.localStorage.getItem("token");
+
+    return config;
+})
+
 export default axiosBaseUrl
