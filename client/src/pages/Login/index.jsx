0ed797e345936504dd8eb02a1f7a155f388196e9
Bektur211 <bekamaasaliev@gmail.com>
2023-01-21 01:22:50 +0600

    add token to localStorage when user is logged in

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
@@ -22,9 +22,19 @@ export const Login = () => {
     mode : "onChange"
   })
 
-  const onSubmit = (values) => {
-    dispatch(fetchAuth(values))
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values))
+
+    if(!data.payload){
+      alert("Не удалось авторизоваться!")
+    }
+
+    if("token" in data.payload){
+      window.localStorage.setItem("token", data.payload.token)
+    }
   }
+
+  // useEffect
 
   if(isAuth){
     return <Navigate to="/"/>
