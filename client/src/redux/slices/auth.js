4187a9767ff6fc67e00e2059a84da47de761fd43
Bektur211 <bekamaasaliev@gmail.com>
2023-01-21 01:23:34 +0600

    create new action to fetch user info

--- client/src/redux/slices/auth.js
+++ client/src/redux/slices/auth.js
@@ -6,6 +6,13 @@ export const fetchAuth = createAsyncThunk("auth/fetchAuth", async (params) => {
     return data;
 })
 
+export const fetchAuthMe = createAsyncThunk("auth/fetchAuthMe", async () => {
+    const {data} = await axiosBaseUrl.get("/auth/profile");
+    console.log(data);
+    return data;
+})
+
+
 const initialState = {
     data: null,
     status: "loading"
@@ -29,6 +36,18 @@ const authSlice = createSlice({
             state.data = action.payload
         },
         [fetchAuth.rejected]: (state)=> {
+            state.status = "error"
+            state.data = null
+        },
+        [fetchAuthMe.pending]: (state)=> {
+            state.status = "loading"
+            state.data = null
+        },
+        [fetchAuthMe.fulfilled]: (state,action)=> {
+            state.status = "laoded"
+            state.data = action.payload
+        },
+        [fetchAuthMe.rejected]: (state)=> {
             state.status = "error"
             state.data = null
         },
