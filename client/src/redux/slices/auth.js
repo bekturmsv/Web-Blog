62efe68a54a7438a614e5df775880815f6f198e1
Bektur211 <bekamaasaliev@gmail.com>
2023-01-21 01:00:24 +0600

    new action to logout

--- client/src/redux/slices/auth.js
+++ client/src/redux/slices/auth.js
@@ -14,6 +14,11 @@ const initialState = {
 const authSlice = createSlice({
     name: "auth",
     initialState,
+    reducers : {
+        logout: (state) => {
+            state.data = null;
+        }
+    },
     extraReducers:{
         [fetchAuth.pending]: (state)=> {
             state.status = "loading"
@@ -32,3 +37,4 @@ const authSlice = createSlice({
 
 export const authReducer = authSlice.reducer
 export const selectIsAuth = state => Boolean(state.auth.data)
+export const { logout } = authSlice.actions
