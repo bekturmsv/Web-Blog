6b79b3b2ec1115ad9f914f6704cd39b1426f19fd
Bektur211 <bekamaasaliev@gmail.com>
2023-01-20 06:59:43 +0600

    using hook useform, collect and send user data to server. If the user is authorized, I get a response from the server with all his data.

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -3,26 +3,55 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-
+import {useForm} from "react-hook-form"
 import styles from "./Login.module.scss";
+import {useDispatch, useSelector} from "react-redux"
+import { fetchAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+
+  const dispatch = useDispatch()
+
+  const { register, handleSubmit, setError, formState : {errors, isValid} } = useForm({
+    defaultValues:{
+      email: "auth@auth.com",
+      password: "1234567"
+    },
+    mode : "onChange"
+  })
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values))
+  }
+
+  console.log(isValid);
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <TextField
         className={styles.field}
         label="E-Mail"
-        error
-        helperText="Неверно указана почта"
+        error={Boolean(errors.email?.message)}
+        helperText={errors.email?.message}
+        {...register("email", {required: "Укажите почту"})}
         fullWidth
+        type="email"
       />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
+      <TextField 
+      className={styles.field} 
+      label="Пароль" 
+      error={Boolean(errors.password?.message)}
+      helperText={errors.password?.message}
+      fullWidth 
+      {...register("password", {required: "Укажите пароль"})}
+      />
+      <Button type="submit" size="large" variant="contained" fullWidth>
         Войти
       </Button>
+    </form>
     </Paper>
   );
 };
