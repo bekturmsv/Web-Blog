8d848b6f41955b0132143f9aa040e52d84588fb8
Bektur211 <bekamaasaliev@gmail.com>
2023-01-23 01:43:12 +0600

    made all instances controllable for easy collection of data from them

--- client/src/pages/AddPost/index.jsx
+++ client/src/pages/AddPost/index.jsx
@@ -6,10 +6,17 @@ import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../redux/slices/auth';
+import { Navigate } from 'react-router-dom';
 
 export const AddPost = () => {
+
+  const isAuth = useSelector(selectIsAuth)
+
   const imageUrl = '';
   const [value, setValue] = React.useState('');
+  const [postObj, setPostObj] = React.useState({title:"", tags: "", value: ""});
 
   const handleChangeFile = () => {};
 
@@ -20,7 +27,7 @@ export const AddPost = () => {
   }, []);
 
   const options = React.useMemo(
-    () => ({
+    () => ({  
       spellChecker: false,
       maxHeight: '400px',
       autofocus: true,
@@ -34,6 +41,11 @@ export const AddPost = () => {
     [],
   );
 
+    if((!window.localStorage.getItem("token")) &&!isAuth){
+      return <Navigate to="/"/>
+    }
+
+console.log(postObj);
   return (
     <Paper style={{ padding: 30 }}>
       <Button variant="outlined" size="large">
@@ -54,9 +66,17 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Заголовок статьи..."
+        value={postObj.title}
+        onChange={e => {setPostObj({...postObj, title:e.target.value})}}
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
+      <TextField 
+      classes={{ root: styles.tags }} 
+      variant="standard" 
+      placeholder="Тэги" 
+      value={postObj.tags}
+      onChange={e => {setPostObj({...postObj, tags:e.target.value})}}
+      fullWidth />
       <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button size="large" variant="contained">
