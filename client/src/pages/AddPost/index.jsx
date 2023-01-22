38f4ee2b93eccb9246fb24dc7a97438d39c88a2f
Bektur211 <bekamaasaliev@gmail.com>
2023-01-23 02:54:34 +0600

    create and send new post to server

--- client/src/pages/AddPost/index.jsx
+++ client/src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -8,15 +8,19 @@ import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import { useSelector } from 'react-redux';
 import { selectIsAuth } from '../../redux/slices/auth';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export const AddPost = () => {
 
   const isAuth = useSelector(selectIsAuth)
-
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
-  const [postObj, setPostObj] = React.useState({title:"", tags: "", imageUrl: ""});
+  const navigate = useNavigate()
+  const [isLoading, setLoading] = useState(false)
+  const [text,setText] = useState("")
+  const [postObj, setPostObj] = React.useState({
+    title:"", 
+    tags: "", 
+    imageUrl: "", 
+  });
 
   const inputImageRef = useRef(null)
 
@@ -34,11 +38,26 @@ export const AddPost = () => {
   };
 
   const onClickRemoveImage =  () => {
+    setPostObj({...postObj, imageUrl: ""})
   };
 
   const onChange = React.useCallback((value) => {
-    setValue(value);
+    setText(value)
   }, []);
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true)
+      const obj = {...postObj,text}
+      console.log(obj);
+      const {data} = await axiosBaseUrl.post("/posts", obj)
+      const id = data._id
+      navigate(`/posts/${id}`)
+
+    } catch (error) {
+      console.warn("Ошибка при создании статьи!")
+    }
+  }
 
   const options = React.useMemo(
     () => ({  
@@ -59,6 +78,8 @@ export const AddPost = () => {
     if((!window.localStorage.getItem("token")) &&!isAuth){
       return <Navigate to="/"/>
     }
+
+
 
   return (
     <Paper style={{ padding: 30 }}>
@@ -91,9 +112,9 @@ export const AddPost = () => {
       value={postObj.tags}
       onChange={e => {setPostObj({...postObj, tags:e.target.value})}}
       fullWidth />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
+        <Button onClick={onSubmit} size="large" variant="contained">
           Опубликовать
         </Button>
         <a href="/">
