58a11c8db469cb70ab4d698d0c11f1d1f20c9471
Bektur211 <bekamaasaliev@gmail.com>
2023-01-24 01:16:51 +0600

    refactoring code

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import Container from "@mui/material/Container";
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 import { Route, Routes } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
+import { fetchAuthMe } from "./redux/slices/auth";
 
 function App() {
 
   const dispatch = useDispatch()
-  const isAuth = useSelector(selectIsAuth)
 
   useEffect(()=> {
     dispatch(fetchAuthMe())
