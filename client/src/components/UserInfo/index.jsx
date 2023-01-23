64761104ce4cb5b63b252e272b5be01c56d4f8c5
Bektur211 <bekamaasaliev@gmail.com>
2023-01-24 01:14:43 +0600

    refactoring code

--- client/src/components/UserInfo/index.jsx
+++ client/src/components/UserInfo/index.jsx
@@ -4,7 +4,7 @@ import styles from './UserInfo.module.scss';
 export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
   return (
     <div className={styles.root}>
-      <img className={styles.avatar} src={avatarUrl || '/noavatar.png'} alt={fullName} />
+      <img className={styles.avatar} src={avatarUrl || ""} alt={fullName} />
       <div className={styles.userDetails}>
         <span className={styles.userName}>{fullName}</span>
         <span className={styles.additional}>{additionalText}</span>
