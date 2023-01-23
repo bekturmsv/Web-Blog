c624380dacbbcf018b08c529e77dfe8f26bd47e4
Bektur211 <bekamaasaliev@gmail.com>
2023-01-24 01:38:05 +0600

    refactoring code

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
@@ -21,5 +21,5 @@ root.render(
         </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
