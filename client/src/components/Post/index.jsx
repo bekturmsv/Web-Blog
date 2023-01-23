41f7a5cc8fd72a85d2ba08581003404863252a2c
Bektur211 <bekamaasaliev@gmail.com>
2023-01-24 01:16:27 +0600

    apply an action to delete a post when user click the delete button

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -9,6 +9,8 @@ import { Link } from 'react-router-dom';
 import styles from './Post.module.scss';
 import { UserInfo } from '../UserInfo';
 import { PostSkeleton } from './Skeleton';
+import {useDispatch}  from "react-redux"
+import { fetchRemovePost } from '../../redux/slices/posts';
 
 export const Post = ({
   id,
@@ -24,11 +26,18 @@ export const Post = ({
   isLoading,
   isEditable,
 }) => {
+
+  const dispatch = useDispatch()
+
   if (isLoading) {
     return <PostSkeleton />;
   }
 
-  const onClickRemove = () => {};
+  const onClickRemove = () => {
+    if(window.confirm("Вы действительно хотите удалить эту статью?")){
+      dispatch(fetchRemovePost(id))
+    }
+  };
 
   return (
     <div className={clsx(styles.root, { [styles.rootFull]: isFullPost })}>
