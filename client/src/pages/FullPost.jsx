f3bb5ca1710e235b211ca1d4fa75051f895e4ea1
Bektur211 <bekamaasaliev@gmail.com>
2023-01-24 01:14:15 +0600

    refactoring code

--- client/src/pages/FullPost.jsx
+++ client/src/pages/FullPost.jsx
@@ -33,7 +33,7 @@ export const FullPost = () => {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={`http://localhost:5000${data.imageUrl}`}
+        imageUrl={data.imageUrl ? `http://localhost:5000${data.imageUrl}` : ""}
         // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={data.user}
         createdAt={data.createdAt}
