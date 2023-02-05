d8294c616b02beb7ede94b0fa18582e9c49dcdec
Bektur211 <bekamaasaliev@gmail.com>
2023-02-05 23:54:20 +0600

    changed the fake picture to the original profile picture

--- client/src/components/UserInfo/index.jsx
+++ client/src/components/UserInfo/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import styles from './UserInfo.module.scss';
+import { Avatar } from "@mui/material";
+import React from "react";
+import styles from "./UserInfo.module.scss";
 
 export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
   return (
     <div className={styles.root}>
-      <img className={styles.avatar} src={avatarUrl || ""} alt={fullName} />
+      <Avatar
+        className={styles.avatar}
+        sx={{ width: 40, height: 40 }}
+        src={`http://localhost:5000${avatarUrl}`}
+      />
       <div className={styles.userDetails}>
         <span className={styles.userName}>{fullName}</span>
         <span className={styles.additional}>{additionalText}</span>
