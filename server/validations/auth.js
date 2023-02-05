afe7948c908fc8d1eb2dbb8562e72dd05ce57e08
Bektur211 <bekamaasaliev@gmail.com>
2023-02-05 23:59:37 +0600

    changing isUrl to isString

--- server/validations/auth.js
+++ server/validations/auth.js
@@ -4,7 +4,7 @@ export const registerValidation =[
     body("email", "Неверный формат почты").isEmail(),
     body("password","Пароль должен быть минимум 7 символов").isLength({min: 7}),
     body("fullName","Укажите имя").isLength({min: 3}),
-    body("avatarUrl","Неверная ссылка на аватарку").optional().isURL(),
+    body("avatarUrl","Неверная ссылка на аватарку").optional().isString(),
 ]
 
 export const loginValidation =[
@@ -15,6 +15,6 @@ export const loginValidation =[
 export const updateValidation = [
     body("fullName", "Укажите новое имя").isLength({min: 3}),
     body("email", "Неверный формат почты").isEmail(),
-    body("avatarUrl","Неверная ссылка на аватарку").optional().isURL(),
+    body("avatarUrl","Неверная ссылка на аватарку").optional().isString(),
 
 ]
