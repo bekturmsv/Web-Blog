cfac0eac6043c9518259f52ce68e1005494fcaf3
Bektur211 <bekamaasaliev@gmail.com>
2023-02-05 23:50:14 +0600

    refactoring code

--- client/src/pages/FullPost.jsx
+++ client/src/pages/FullPost.jsx
@@ -1,31 +1,29 @@
-import React, {useState, useEffect} from "react";
-import  {useParams} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Post } from "../components/Post";
-import { Index } from "../components/AddComment";
-import { CommentsBlock } from "../components/CommentsBlock";
 import axiosBaseUrl from "../axios";
-import ReactMarkdown from "react-markdown"
+import ReactMarkdown from "react-markdown";
 
 export const FullPost = () => {
-  const [data,setData] = useState()
-  const [isLoading, setLoading] = useState(true )
-  const {id} = useParams();
+  const [data, setData] = useState();
+  const [isLoading, setLoading] = useState(true);
+  const { id } = useParams();
 
-  useEffect(()=> {
+  useEffect(() => {
     axiosBaseUrl
-    .get(`/posts/${id}`)
-    .then(res => {
-      setData(res.data)
-      setLoading(false)
-    })
-    .catch(err => {
-      console.warn(err);
-      alert("Ошибка при получении статьи")
-    })
-  },[])
+      .get(`/posts/${id}`)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении статьи");
+      });
+  }, []);
 
-  if(isLoading){
-    return <Post isLoading={isLoading} isFullPost /> 
+  if (isLoading) {
+    return <Post isLoading={isLoading} isFullPost />;
   }
 
   return (
@@ -34,14 +32,14 @@ export const FullPost = () => {
         id={data._id}
         title={data.title}
         imageUrl={data.imageUrl ? `http://localhost:5000${data.imageUrl}` : ""}
-        // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
         commentsCount={3}
         tags={data.tags}
-        isFullPost>
-        <ReactMarkdown children={data.text}  />
+        isFullPost
+      >
+        <ReactMarkdown children={data.text} />
       </Post>
       {/* <CommentsBlock
         items={[
