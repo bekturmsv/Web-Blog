fb98d46ec2a428e8047aca4211f0c3efca5e5183
Bektur211 <bekamaasaliev@gmail.com>
2023-02-05 21:38:34 +0600

    changing min text length

--- server/validations/post.js
+++ server/validations/post.js
@@ -2,7 +2,7 @@ import {body} from "express-validator"
 
 export const postCreateValidation =[
     body("title", "Введите заголовок статьи").isLength({min:3}).isString(),
-    body("text","Введите текст статьи").isLength({min: 10}).isString(),
+    body("text","Введите текст статьи").isLength({min: 5}).isString(),
     body("tags","Неверный формат тэгов (укажите массив)").optional().isString(),
     body("imageUrl","Неверная ссылка на изображение").optional().isString(),
 ]
