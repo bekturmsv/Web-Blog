d279ef2b8d6af7ab41c7d09c527362694919ec47
Bektur211 <bekamaasaliev@gmail.com>
2023-03-12 11:26:32 +0600

    update project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,7 +47,7 @@ export const Home = () => {
                 viewsCount={obj.viewsCount}
                 commentsCount={"not working yet"}
                 tags={obj.tags}
-                isEditable={userData?._id === obj.user._id}
+                // isEditable={userData?._id === obj.user._id}
               />
             )
           )}
